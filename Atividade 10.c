3667b89d9887fa4ddaef647444f957077ecb2792
Mario Henrique <mhliquinho@gmail.com>
2022-07-04 09:14:10 -0300

    Uploading Atividade 10 Arquivos2

---
+++ Atividade 10.c
@@ -0,0 +1,45 @@
+//Fgets and Fputs
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+	FILE *point1, *point2;
+	point1 = fopen("Atividade 10.txt", "w");
+	while(point1 == NULL){
+		printf("Sorry, we can´t open paste ArquivoString.txt\n");
+		exit(0);
+	}
+	char c[50];
+	printf("Write a phrase\n");
+	fgets(c, 50, stdin);
+	fflush(stdin);
+	
+	
+	if(1){
+		char aux = fputs(c, point1);
+		if(aux == EOF){
+			printf("Error! We can´t put phrase in ArquivoString.txt");
+			exit(0);
+		}	
+	}else{
+		printf("Error!!");
+	}
+	fclose(point1);
+	
+	system("cls");
+	
+	point2 = fopen("ArquivoString.txt", "r");
+	while(point2 == NULL){
+		printf("Sorry, we can´t open paste ArquivoString.txt\n");
+		exit(0);
+	}
+	
+	char b[50];
+	if(1){
+		fgets(b, 50, point2);
+		printf("%s", b);
+	}else{
+		printf("Error!!");
+	}
+	
+	fclose(point2);
+}
